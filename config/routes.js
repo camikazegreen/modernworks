b5dee69845180b28f9d392d6541462f907758035
camikazegreen <camikazegreen@gmail.com>
2015-03-30 16:58:04 -0700

    Updated the route to add /user

--- config/routes.js
+++ config/routes.js
@@ -39,6 +39,9 @@ module.exports.routes = {
   '/client': {
     view: 'client'
   },
+  '/user': {
+    view: 'user'
+  }
   /***************************************************************************
   *                                                                          *
   * Custom routes here...                                                    *
