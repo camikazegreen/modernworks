925fc5031f1a5a1bec448169e8dbc1e221823cf1
camikazegreen <camikazegreen@gmail.com>
2015-03-31 02:39:49 +0000

    added capacity and PRO apis

---
+++ api/controllers/CapacityController.js
@@ -0,0 +1,11 @@
+/**
+ * CapacityController
+ *
+ * @description :: Server-side logic for managing capacities
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/controllers/PROController.js
@@ -0,0 +1,11 @@
+/**
+ * PROController
+ *
+ * @description :: Server-side logic for managing PROS
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
