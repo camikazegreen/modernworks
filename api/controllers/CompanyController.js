8dbd275c97e9439de0cb75d13dfdba632b08cf5c
camikazegreen <camikazegreen@gmail.com>
2015-03-31 13:01:32 -0700

    Added a bunch of controllers

--- api/controllers/CompanyController.js
+++ api/controllers/CompanyController.js
@@ -6,6 +6,85 @@
  */
 
 module.exports = {
-	
+
+
+
+  /**
+   * `CompanyController.index()`
+   */
+  index: function (req, res) {
+    return res.json({
+      todo: 'index() is not implemented yet!'
+    });
+  },
+/**
+ * 'CompanyController.new()'
+ */
+  new: function(req,res){
+    res.send(
+      '<form action="http://107.170.53.5:1337/company/create" enctype="multipart/form-data" method="post">'+
+      'name:<input type="text" name="name"><br>'+
+      'email:<input type="text" name="email"><br>'+
+      'password:<input type="text" name="password"><br>'+
+      '<input type="submit" value="submit">'+
+      '</form>'
+      )
+  },
+
+  /**
+   * `CompanyController.create()`
+   */
+  create: function (req, res) {
+    var params = req.params.all()
+    var name = params.name;
+    Company.create({name:name}, function(err, company){
+      if (err){
+        res.send(err);
+      }
+      else{
+        return res.send(company.id);
+      }
+    });
+  },
+
+
+  /**
+   * `CompanyController.show()`
+   */
+  show: function (req, res) {
+    var id = req.param('id');
+    Company.findById(id, function(err, company){
+      if (err){
+        res.send(err);
+      }
+      else{
+        res.send(company);
+      }
+    });
+  },
+
+
+
+  /**
+   * `CompanyController.edit()`
+   */
+  edit: function (req, res) {
+    return res.json({
+      todo: 'edit() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `CompanyController.delete()`
+   */
+  delete: function (req, res) {
+    return res.json({
+      todo: 'delete() is not implemented yet!'
+    });
+  }
 };
+
+
+
 
--- api/controllers/DepositDetailController.js
+++ api/controllers/DepositDetailController.js
@@ -6,6 +6,91 @@
  */
 
 module.exports = {
-	
+
+
+
+  /**
+   * `DepositDetailController.index()`
+   */
+  index: function (req, res) {
+    return res.json({
+      todo: 'index() is not implemented yet!'
+    });
+  },
+/**
+ * 'DepositDetailController.new()'
+ */
+  new: function(req,res){
+    res.send(
+      '<form action="http://107.170.53.5:1337/depositDetail/create" enctype="multipart/form-data" method="post">'+
+      'name:<input type="text" name="name"><br>'+
+      'email:<input type="text" name="email"><br>'+
+      'password:<input type="text" name="password"><br>'+
+      '<input type="submit" value="submit">'+
+      '</form>'
+      )
+  },
+
+  /**
+   * `DepositDetailController.create()`
+   */
+  create: function (req, res) {
+    var params = req.params.all()
+    var name = params.name;
+    var account = params.account;
+    var routing = params.routing;
+    var bankName = params.bankName;
+    var checking = params.checking;
+    var attachment = params.attachment;
+    var user = params.user;
+    DepositDetail.create({name:name,account:account,routing:routing,bankName:bankName,checking:checking,attachment:attachment,user:user}, function(err, depositDetail){
+      if (err){
+        res.send(err);
+      }
+      else{
+        return res.send(depositDetail.id);
+      }
+    });
+  },
+
+
+  /**
+   * `DepositDetailController.show()`
+   */
+  show: function (req, res) {
+    var id = req.param('id');
+    DepositDetail.findById(id, function(err, depositDetail){
+      if (err){
+        res.send(err);
+      }
+      else{
+        res.send(depositDetail);
+      }
+    });
+  },
+
+
+
+  /**
+   * `DepositDetailController.edit()`
+   */
+  edit: function (req, res) {
+    return res.json({
+      todo: 'edit() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `DepositDetailController.delete()`
+   */
+  delete: function (req, res) {
+    return res.json({
+      todo: 'delete() is not implemented yet!'
+    });
+  }
 };
+
+
+
 
