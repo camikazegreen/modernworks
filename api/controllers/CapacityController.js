6b0f4048fd26ec5a2177748f7138c2a2b6b33956
camikazegreen <camikazegreen@gmail.com>
2015-03-31 11:00:45 -0700

    Added a few controllers

--- api/controllers/CapacityController.js
+++ api/controllers/CapacityController.js
@@ -6,6 +6,83 @@
  */
 
 module.exports = {
-	
+
+
+
+  /**
+   * `CapacityController.index()`
+   */
+  index: function (req, res) {
+    return res.json({
+      todo: 'index() is not implemented yet!'
+    });
+  },
+/**
+ * 'CapacityController.new()'
+ */
+  new: function(req,res){
+    res.send(
+      '<form action="http://107.170.53.5:1337/capacity/create" enctype="multipart/form-data" method="post">'+
+      'name:<input type="text" name="name"><br>'+
+      'email:<input type="text" name="email"><br>'+
+      'password:<input type="text" name="password"><br>'+
+      '<input type="submit" value="submit">'+
+      '</form>'
+      )
+  },
+
+  /**
+   * `CapacityController.create()`
+   */
+  create: function (req, res) {
+    var params = req.params.all()
+    var name = params.name;
+    Capacity.create({name:name}, function(err, capacity){
+      if (err){
+        res.send(err);
+      }
+      else{
+        return res.send(capacity.id);
+      }
+    });
+  },
+
+
+  /**
+   * `CapacityController.show()`
+   */
+  show: function (req, res) {
+    var id = req.param('id');
+    Capacity.findById(id, function(err, capacity){
+      if (err){
+        res.send(err);
+      }
+      else{
+        res.send(capacity);
+      }
+    });
+  },
+
+
+
+  /**
+   * `CapacityController.edit()`
+   */
+  edit: function (req, res) {
+    return res.json({
+      todo: 'edit() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `CapacityController.delete()`
+   */
+  delete: function (req, res) {
+    return res.json({
+      todo: 'delete() is not implemented yet!'
+    });
+  }
 };
+
 
--- api/controllers/CheckDetailController.js
+++ api/controllers/CheckDetailController.js
@@ -6,6 +6,85 @@
  */
 
 module.exports = {
-	
+
+
+
+  /**
+   * `CheckDetailController.index()`
+   */
+  index: function (req, res) {
+    return res.json({
+      todo: 'index() is not implemented yet!'
+    });
+  },
+/**
+ * 'CheckDetailController.new()'
+ */
+  new: function(req,res){
+    res.send(
+      '<form action="http://107.170.53.5:1337/checkDetail/create" enctype="multipart/form-data" method="post">'+
+      'name:<input type="text" name="name"><br>'+
+      'email:<input type="text" name="email"><br>'+
+      'password:<input type="text" name="password"><br>'+
+      '<input type="submit" value="submit">'+
+      '</form>'
+      )
+  },
+
+  /**
+   * `CheckDetailController.create()`
+   */
+  create: function (req, res) {
+    var params = req.params.all()
+    var payee = params.payee;
+    var address = params.address;
+    var user = params.user;
+    CheckDetail.create({payee:payee,address:address,user:user}, function(err, address){
+      if (err){
+        res.send(err);
+      }
+      else{
+        return res.send(checkDetail.id);
+      }
+    });
+  },
+
+
+  /**
+   * `CheckDetailController.show()`
+   */
+  show: function (req, res) {
+    var id = req.param('id');
+    CheckDetail.findById(id, function(err, checkDetail){
+      if (err){
+        res.send(err);
+      }
+      else{
+        res.send(checkDetail);
+      }
+    });
+  },
+
+
+
+  /**
+   * `CheckDetailController.edit()`
+   */
+  edit: function (req, res) {
+    return res.json({
+      todo: 'edit() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `CheckDetailController.delete()`
+   */
+  delete: function (req, res) {
+    return res.json({
+      todo: 'delete() is not implemented yet!'
+    });
+  }
 };
+
 
