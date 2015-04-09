3147f04403cb912971f92049b3365277ff29b49b
camikazegreen <camikazegreen@gmail.com>
2015-04-09 13:13:10 -0700

    works without bucket?

--- api/controllers/SongController.js
+++ api/controllers/SongController.js
@@ -24,9 +24,9 @@ module.exports = {
 	upload: function (req,res){
 		req.file('songMP3').upload({
 			adapter: require('skipper-s3'),
-			key: 'AKIAJ37U65YDQAQ7FRTA'
-			secret: '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'
-			bucket: 'mw-songs'
+			key: 'AKIAJ37U65YDQAQ7FRTA',
+			secret: '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',
+			// bucket: 'mw-songs',
 			region: 'Oregon'
 		},function whenDone(err,uploadedFiles){
 			if (err){
