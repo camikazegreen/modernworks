fe2be2f3ac31e2f92dcf305e782400088e737c6d
camikazegreen <camikazegreen@gmail.com>
2015-04-09 12:40:00 -0700

    added S3 as an upload destination

--- api/controllers/SongController.js
+++ api/controllers/SongController.js
@@ -23,6 +23,10 @@ module.exports = {
 	},
 	upload: function (req,res){
 		req.file('songMP3').upload({
+			adapter: require('skipper-s3'),
+			key: 'AKIAJ37U65YDQAQ7FRTA',
+			bucket: 'mw-songs',
+			secret: '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'
 		},function whenDone(err,uploadedFiles){
 			if (err){
 				return res.negotiate(err);
