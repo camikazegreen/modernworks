79fc6e2e25c447a25f8150a5de4ee170a53d662a
camikazegreen <camikazegreen@gmail.com>
2015-04-13 17:26:57 -0700

    Song details

--- assets/js/songpuller.js
+++ assets/js/songpuller.js
@@ -3,13 +3,29 @@
     document.querySelector('input[type="file"]').onchange = function(e) {
       id3(this.files[0], function(err, tags) {
         console.log(tags)  // tags now contains your ID3 tags 
-        var arrayBufferView = new Uint16Array(tags.v2.image.data);
-        var blob = new Blob([arrayBufferView],{type:"image/jpeg"});
-        var urlCreator = window.URL || window.webkitURL;
-        var imageUrl = urlCreator.createObjectURL(blob);
-        var img = document.querySelector("#coverArt");
-        img.src = imageUrl;
-
+        
+        function thisSongDeets(){
+           var arrayBufferView = new Uint16Array(tags.v2.image.data);
+            var blob = new Blob([arrayBufferView],{type:"image/jpeg"});
+            var urlCreator = window.URL || window.webkitURL;
+            var imageUrl = urlCreator.createObjectURL(blob);
+            // var img = document.querySelector("#coverArt");
+            // img.src = imageUrl; 
+            var albumArt = document.createElement('img');
+            albumArt.setAttribute('src',imageUrl);
+            var title = document.createElement('h3');
+            title.innerHTML = tags.title;
+            var artist = document.createElement('h4');
+            artist.innerHTML = 'by ' + tags.artist;
+            var album = document.createElement('h4');
+            album.innerHTML = 'on ' + tags.album;
+            var infoBox = document.createElement('div');
+            infoBox.appendChild(albumArt);
+            infoBox.appendChild(title);
+            infoBox.appendChild(artist);
+            infoBox.appendChild(album);
+            document.getElementById('songDetails').appendChild(infoBox);
+        }()
 
     });
 }
