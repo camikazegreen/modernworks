a4d4fc7a058ca19fd9b23b5008ab4838f0908e35
camikazegreen <camikazegreen@gmail.com>
2015-04-15 14:53:06 -0700

    big moves, adding a player

--- assets/js/songpuller.js
+++ assets/js/songpuller.js
@@ -3,12 +3,14 @@ var j=0;//definied outside of the loop so that each progress bar will have a uni
     document.querySelector('input[type="file"]').onchange = function(e) {
         i=0;
         while(i<this.files.length){
+            var h=j;
       id3(this.files[i], function(err, tags) {
         console.log(tags)  // tags now contains your ID3 tags 
         
         function thisSongDeets(){
 
             var row = document.getElementById('songDetailTable').insertRow(1);
+            row.setAttribute('id','row'+h);
            var arrayBufferView = new Uint8Array(tags.v2.image.data);
             var blob = new Blob([arrayBufferView],{type:"image/jpeg"});
             var urlCreator = window.URL || window.webkitURL;
@@ -32,7 +34,7 @@ var j=0;//definied outside of the loop so that each progress bar will have a uni
             infoBox.appendChild(artist);
             infoBox.appendChild(album);
             var progress = document.createElement('progress');
-            progress.setAttribute('id','progress'+j);
+            progress.setAttribute('id','progress'+h);
             progBox.appendChild(progress);
             postSong(tags);
 
@@ -44,16 +46,6 @@ var j=0;//definied outside of the loop so that each progress bar will have a uni
         var file = document.getElementById('fileupload').files[0];
         var formData = new FormData();
         formData.append('songMP3',file,file.name);
-        // var xhr = new XMLHttpRequest();
-        // xhr.open('POST', 'http://107.170.53.5:1337/song', true);
-        // xhr.onload = function(){
-        //     if (xhr.status===200){
-        //         console.log("we're all good");
-        //     } else {
-        //         console.log("an error occurred");
-        //     }
-        // };
-        // xhr.send(formData);
     $.ajax({
         url:'http://107.170.53.5:1337/song?title='+tags.title+'&artist='+tags.artist+'&album='+tags.album+'&year='+tags.year,
         type: 'POST',
@@ -69,11 +61,16 @@ var j=0;//definied outside of the loop so that each progress bar will have a uni
         contentType: false,
         processData: false
     }).done(function(result){
-        console.log(result.status);
+        console.log(result.status.id);
+        row=document.getElementById('row'+h);
+        var playerBox = row.insertCell(3);
+        var player = document.createElement('audio');
+        player.setAttribute('src','song/songMP3?id='+result.status.id);
+        playerBox.appendChild(player);
     });
     function progressHandling(e){
         if(e.lengthComputable){
-            $('#progress'+j).attr({value:e.loaded,max:e.total});
+            $('#progress'+h).attr({value:e.loaded,max:e.total});
         }
     }
 };
