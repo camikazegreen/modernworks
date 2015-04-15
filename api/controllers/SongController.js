2cef1ae0d7ca5aa61a9ff7407031ee7c35560b3a
camikazegreen <camikazegreen@gmail.com>
2015-04-15 14:08:07 -0700

    ?

--- api/controllers/SongController.js
+++ api/controllers/SongController.js
@@ -46,10 +46,10 @@ module.exports = {
  				  Song.create({
 					songFd: uploadedFiles[0].fd,
 					songMP3url: uploadedFiles[0].extra.Location,
-					// title: tags.title,
-					artist: tags.artist
-					// album: 'billys album',
-					// year: 'now'
+					title: tags.title,
+					artist: tags.artist,
+					album: 'billys album',
+					year: 'now'
 				},function(err,song){
 					res.writeHead(200, { 'Content-Type': 'application/json' });
 					res.write(JSON.stringify({ status: song }));
