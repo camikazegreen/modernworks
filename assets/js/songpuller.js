fe20c6ff4841543b016a88ef5cbd994d9f452a72
camikazegreen <camikazegreen@gmail.com>
2015-04-16 11:06:55 -0700

    fix progress

--- assets/js/songpuller.js
+++ assets/js/songpuller.js
@@ -38,7 +38,7 @@ var j=0;//definied outside of the loop so that each progress bar will have a uni
             infoBox.appendChild(artist);
             infoBox.appendChild(album);
             var progress = document.createElement('progress');
-            progress.setAttribute('id','progress'+j);
+            progress.setAttribute('id','progress'+h);
             progBox.appendChild(progress);
             postSong(tags);
 
