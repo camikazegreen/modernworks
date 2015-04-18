2568dfb367f6b38fc86b7c6fdac9edeeb7b03290
camikazegreen <camikazegreen@gmail.com>
2015-04-17 17:04:57 -0700

    weird behavior

--- api/controllers/SongController.js
+++ api/controllers/SongController.js
@@ -35,8 +35,9 @@ module.exports = {
     var mbid = ''; //empty string for MusicBrainz ID
     nb.search('work',{artist:tags.artist,work:tags.title}, function(err, response){
     	if(response.works){
-    		mbid=response.works[0].id;
-    		console.log(response.works[0].relations[0])
+    		console.log(response.works);
+    		// mbid=response.works[0].id;
+    		// console.log(response.works[0].relations[0])
     	}
     })
    // console.log(tags);
