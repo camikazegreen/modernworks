c9c8460dea126a48f4424e931edf246a84814f8c
camikazegreen <camikazegreen@gmail.com>
2015-04-20 13:48:29 -0700

    assigned progress handlers

--- assets/js/songpuller.js
+++ assets/js/songpuller.js
@@ -36,7 +36,7 @@ var j=0;//definied outside of the loop so that each progress bar will have a uni
             infoBox.appendChild(artist);
             infoBox.appendChild(album);
             var progress = document.createElement('progress');
-            progress.setAttribute('id','progress'+h);
+            progress.setAttribute('id','progress'+tags.title);
             progBox.appendChild(progress);
             postSong(tags);
 
@@ -48,6 +48,11 @@ var j=0;//definied outside of the loop so that each progress bar will have a uni
         var file = document.getElementById('fileupload').files[0];
         var formData = new FormData();
         formData.append('songMP3',file,file.name);
+            function progressHandling(e){
+        if(e.lengthComputable){
+            $('#progress'+tags.title).attr({value:e.loaded,max:e.total});
+        }
+    }
     $.ajax({
         url:'http://107.170.53.5:1337/song?title='+encodeURIComponent(tags.title)+'&artist='+encodeURIComponent(tags.artist)+'&album='+encodeURIComponent(tags.album),
         type: 'POST',
@@ -83,11 +88,11 @@ var j=0;//definied outside of the loop so that each progress bar will have a uni
 
         echoBox.innerHTML=echoHTML;
     });
-    function progressHandling(e){
-        if(e.lengthComputable){
-            $('#progress'+h).attr({value:e.loaded,max:e.total});
-        }
-    }
+    // function progressHandling(e){
+    //     if(e.lengthComputable){
+    //         $('#progress'+h).attr({value:e.loaded,max:e.total});
+    //     }
+    // }
 };
 
  i++;j++;
