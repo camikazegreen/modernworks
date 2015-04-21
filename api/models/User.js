e814a07e4815971614f35b34625ab435d7dbd73f
camikazegreen <camikazegreen@gmail.com>
2015-04-21 18:41:40 +0000

    adding all the things

--- api/models/User.js
+++ api/models/User.js
@@ -1,85 +1,12 @@
-/**
-* User.js
-*
-* @description :: TODO: You might write a short summary of how this model works and what it represents here.
-* @docs        :: http://sailsjs.org/#!documentation/models
-*/
-
-module.exports = {
+var User = {
+  // Enforce model schema in the case of schemaless databases
+  schema: true,
 
   attributes: {
-    name:{
-      type:'string',
-      unique: true
-    },
-    email:{
-      type:'email',
-      unique:true
-    },
-    password:{
-      type:'string'
-    },
-    phone:{
-      type: 'string'
-    },
-    address:{
-      model: 'address'
-    },
-    digital:{
-      type: 'boolean'
-    },
-    deposit:{
-      type: 'boolean'
-    },
-    depositDetail:{
-      model: 'depositDetail'
-    },
-    checkDetail:{
-      model: 'checkDetail'
-    },
-    company:{
-      model: 'company'
-    },
-    publisher:{
-      model:'publisher'
-    },
-    isComposer:{
-      type: 'boolean'
-    },
-    composer:{
-      model: 'composer'
-    },
-    accountManager:{
-      model: 'user'
-    },
-    isAccountManager:{
-      type: 'boolean'
-    },
-    accountsManaged:{
-      collection:'user',
-      via:'accountManager'
-    },
-    startDate:{
-      type:'date'
-    },
-    expiryDate:{
-      type:'date'
-    },
-    finder:{
-      model:'user'
-    },
-    finderClients:{
-      collection:'user',
-      via:'finder'
-    },
-    cc:{
-      model:'user'
-    },
-    ccFor:{
-      collection:'user',
-      via:'cc'
-    }
-
+    username  : { type: 'string', unique: true },
+    email     : { type: 'email',  unique: true },
+    passports : { collection: 'Passport', via: 'user' }
   }
 };
 
+module.exports = User;
