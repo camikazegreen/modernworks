210becdf75f4a8973757f3f9a7d7150237560aa8
camikazegreen <camikazegreen@gmail.com>
2015-05-04 15:20:57 -0700

    log the stream

--- api/controllers/SongController.js
+++ api/controllers/SongController.js
@@ -75,8 +75,8 @@ module.exports = {
    		console.log(error);
    	} else if(typeof response.songs[0] === 'undefined'){
    		console.log('no songs found in echonest, analyzing...');
-   		console.log(req.file('songMP3'));
-   		myNest('track/upload').post(req.file('songMP3'),function(err,response){
+   		console.log(req.file('songMP3')._files[0].stream);
+   		myNest('track/upload').post(req.file('songMP3')._files[0].stream,function(err,response){
    			console.log(response);
    		})
    	} else if(response.songs[0]){
