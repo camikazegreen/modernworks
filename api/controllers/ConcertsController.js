b6b573b16858cdbe3e98cd7f56bf06b2da01d4cf
camikazegreen <camikazegreen@gmail.com>
2015-05-12 17:33:43 +0000

    adding concerts api

---
+++ api/controllers/ConcertsController.js
@@ -0,0 +1,11 @@
+/**
+ * ConcertsController
+ *
+ * @description :: Server-side logic for managing concerts
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
