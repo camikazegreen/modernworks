b16ba36c78337c1a6531a8e8b89b07e4dc02d910
camikazegreen <camikazegreen@gmail.com>
2015-05-12 12:44:03 -0700

    adding song kick api access

--- api/controllers/ConcertsController.js
+++ api/controllers/ConcertsController.js
@@ -6,14 +6,47 @@
  */
 var apikeys = require('./apikeys.js');
 var artists = require('../../assets/roster.js');
+var http = require('http');
 
 module.exports = {
 	
 	index: function (req, res){
 		// console.log(artists);
-		var concerts = [];
+		var concerts = {};
+		concerts.newyork = [];
+		concerts.losangeles = [];
+		concerts.nashville = [];
+		concerts.tucson = [];
+		concerts.phoenix = [];
 		var apikey = apikeys.songkickkeys[0].apikey;
-		console.log(apikey);
+		var newYork = "sk:7644";
+		var losAngeles = "sk:17835"; 
+		var nashville = "sk:11104";
+		var tucson = "sk:10046";
+		var phoenix = "sk:23068";
+		// http://api.songkick.com/api/3.0/events.xml?apikey=KEY&location=sk:2846&artist_name=fleet+foxes
+
+var getConcerts(city,artist){
+	var options = {
+		host: 'api.songkick.com',
+		path: '/api/3.0/events.json?apikey='+apikey+'&location='+city+'&artist_name='+artist
+	};
+
+	var callback = function(response){
+		var str = '';
+
+		response.on('data',function(chunk){
+			str += chunk;
+		});
+
+		response.on('end', function(){
+			console.log(str);
+		});
+	}
+	http.request(options, callback).end();
+}
+getConcerts(phoenix,"jakubi");
+
 		return res.view('concerts',{
 			artists: artists,
 			concerts: concerts
