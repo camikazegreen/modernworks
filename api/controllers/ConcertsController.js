9ac5ad668c14daf95943bb255808d7e083f27746
camikazegreen <camikazegreen@gmail.com>
2015-05-12 10:37:15 -0700

    adding index

--- api/controllers/ConcertsController.js
+++ api/controllers/ConcertsController.js
@@ -4,8 +4,15 @@
  * @description :: Server-side logic for managing concerts
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
+var apikeys= require('./apikeys.js');
 
 module.exports = {
+	
+	index: function (req, res){
+		return res.json({
+			todo: 'index() is not implemented yet'
+		});
+	}
 	
 };
 
