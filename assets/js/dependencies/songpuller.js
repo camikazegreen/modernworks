7d292fbe1a26e69e7d51126562a6899ea3390a08
camikazegreen <camikazegreen@gmail.com>
2015-05-27 15:49:21 -0700

    missing paren

--- assets/js/dependencies/songpuller.js
+++ assets/js/dependencies/songpuller.js
@@ -27,7 +27,7 @@ var j=0;//definied outside of the loop so that each progress bar will have a uni
                 }
                 return true;
             }
-            if(!isValid(picture.data){picture.data="jpg"}
+            if(!isValid(picture.data)){picture.data="jpg"}
             imageUrl = URL.createObjectURL(new Blob([picture.data],{'type':'image/'+picture.format}));
             } else {
              imageUrl='/images/MW-small.png'
